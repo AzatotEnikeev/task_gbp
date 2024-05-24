89596daf39b15215f85957fb211a7a3226f8aca7
AzatE <marateni@gmail.com>
2024-05-24 10:19:34 +0500

    add for 4_task path in test

--- task4/test.py
+++ task4/test.py
@@ -8,10 +8,10 @@ def test_task4_delete_files_elder_n_days():
     # Получаем текущее время
     current_time = time.time()
     # Устанавливаем желаемую дату и время модификации
-    creation_time = current_time - - 3600 * 24 * 8  # Вычитаем (в секундах)
+    creation_time = current_time - 3600 * 24 * 8  # Вычитаем (в секундах)
     modification_time = current_time - 3600 * 24 * 8  # Вычитаем  (в секундах)
     # Устанавливаем дату и время модификации файла
-    os.utime('C:\\Users\\1\\PycharmProjects\\Task4\\test\\test_for_del.txt', (creation_time, modification_time))
-    assert(main.delete_files_elder_n_days('C:\\Users\\1\\PycharmProjects\\Task4\\test', 7) is True)
+    os.utime('test\\test_for_del.txt', (creation_time, modification_time))
+    assert(main.delete_files_elder_n_days(os.path.abspath(os.curdir + '\\test'), 7) is True)
 
 
